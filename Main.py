372c697e07ea2f7ee278196b5cd4eaaf2defe979
Shaochen <shaohockey@gmail.com>
2021-05-03 20:39:22 -0600

    basic functionality and math done

--- Main.py
+++ Main.py
@@ -3,17 +3,33 @@ from ast import literal_eval
 
 import numpy as np
 from pip._vendor.distlib.compat import raw_input
+from Model import Model
 
 
 class Main:
+    degree = 0
     def get_points(self):
         try:
             coords = [literal_eval(coord) for coord in raw_input("Please enter your points Ex: (3,0) (0,0) ").split()]
             for coord in coords:
                 print(coord[0])
+            self.degree = int(input("What degree polynomial: "))
         except ValueError:
             print("Please enter the coordinates in the format mentioned")
             exit()
+        
+        self.create_lists(coords)
+    
+    def create_lists(self, data):
+        xList = []
+        yList = []
+        for coord in data:
+            xList.append(coord[0])
+            yList.append(coord[1])
+        print(xList)
+        print(yList)    
+        model = Model(xList, yList, self.degree)
+        model.calculate_result()
 
 main = Main()
 main.get_points()
---
+++ Model.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class Model:
+    xMatrix = []
+    yMatrix = []
+    degree = 0
+    
+    def __init__(self, xMatrix, yMatrix, degree):
+        self.xMatrix = xMatrix
+        self.yMatrix = yMatrix
+        self.degree = degree
+    
+    def create_matrix(self):
+        A = np.vander(self.xMatrix, (self.degree+1), increasing = True)
+        print(A)
+        return A
+
+    def calculate_result(self):
+        A = self.create_matrix()
+        yMatrix = np.matrix(self.yMatrix).T
+        print(yMatrix)
+        c = np.linalg.inv(A.T @ A) @ (A.T @ yMatrix)
+        print(c)
